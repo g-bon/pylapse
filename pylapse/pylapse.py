9dd996feca166c65e88e9fbf5ebaaa0d9dc03ed3
Gabriele Bonetti <gabriele.bonetti@gmail.com>
2018-01-03 00:19:14 +0100

    relative import for configuration

--- pylapse/pylapse.py
+++ pylapse/pylapse.py
@@ -1,19 +1,19 @@
 import asyncio
 from functools import partial
-import pylapse.configuration as config
+from . import configuration
 from .periodictask import PeriodicTask
 from .utils import get_image, create_timelapse
 
 
 def main():
-    for webcam_name, webcam_params in config.webcams.items():
+    for webcam_name, webcam_params in configuration.webcams.items():
         # Create async tasks for image scraping
         PeriodicTask(func=partial(get_image, webcam_name, webcam_params["url"]))
 
         # Create async tasks for timelapse creation
-        if config.create_timelapse:
+        if configuration.create_timelapse:
             PeriodicTask(func=partial(create_timelapse, webcam_name),
-                         interval=config.timelapse_creation_interval)
+                         interval=configuration.timelapse_creation_interval)
 
     try:
         asyncio.get_event_loop().run_forever()
